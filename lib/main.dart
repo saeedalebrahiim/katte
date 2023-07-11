f804871b50ef25c959a8b6a6afd184d448a09fee
hamedjava <hamedpouyanfar1987@gmail.com>
2023-07-11 11:01:40 +0330

    add quama in multi provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:delivery/initscreen.dart';
 import 'package:delivery/model/db/shop_card_entity.dart';
 import 'package:delivery/routs/routs.dart';
-import 'package:delivery/view/pages/home/indexscreen.dart';
 import 'package:delivery/view/provider/home_index.dart';
 import 'package:delivery/view/provider/index_card.dart';
 import 'package:flutter/material.dart';
@@ -11,13 +10,15 @@ import 'package:provider/provider.dart';
 Future<void> main() async {
   await Hive.initFlutter("KatteDb");
   Hive.registerAdapter(ShopCardEntityAdapter());
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (_) => IndexCardState()),
-      ChangeNotifierProvider(create: (_) => HomeIndexProvider())
-    ],
-    child: const MyApp(),
-  ));
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => IndexCardState()),
+        ChangeNotifierProvider(create: (_) => HomeIndexProvider())
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
